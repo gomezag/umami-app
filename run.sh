b0727185b47aca28419cc139665414f7be11c416
Agustin Gomez Mansilla <agustin.gomez.mansilla@gmail.com>
2020-06-14 13:31:45 +0200

    .

--- run.sh
+++
@@ -1,3 +0,0 @@
-cd /volume1/web/umami-app
-source umami-app/bin/activate
-gunicorn --certfile /usr/syno/etc/certificate/system/default/cert.pem --keyfile /usr/syno/etc/certificate/system/default/privkey.pem -b 127.0.0.1:8001 backend.wsgi:application --daemon
